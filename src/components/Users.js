ef391f1d4c854fa901e6535f6eee8011accab8b9
RileyManda <rileymanda69@gmail.com>
2023-07-24 22:20:08 +0200

    Implemented users map

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import  { isLoaded, setError } from './store/users/usersSlice';
+import React from "react";
+import { useSelector } from "react-redux";
 
 export default function Users() {
-
   const isLoading = useSelector((state) => state.users.isLoading);
   const error = useSelector((state) => state.users.error);
+  const users = useSelector((state) => state.users.users);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -15,5 +14,16 @@ export default function Users() {
     return <div>Error: {error}</div>;
   }
 
-  return <div>Userslist</div>;
+  return (
+    <div>
+      <h1>Users List</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.firstName} {user.lastName}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- src/store/users/usersSlice.js
+++ src/store/users/usersSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   users: [],
@@ -7,7 +7,7 @@ const initialState = {
 };
 
 export const usersSlice = createSlice({
-  name: "users",
+  name: 'users',
   initialState,
   reducers: {
     isLoaded: (state) => {
